38dd50c5f8f1b1428e347e7344a3680d738b3d9b
Serge-weihao <40890323+Serge-weihao@users.noreply.github.com>
2019-12-26 23:09:21 +0800

    Add files via upload

---
+++ networks/CC.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Softmax
+print("CC Moudle")
+def INF(B,H,W):
+     return -torch.diag(torch.tensor(float("inf")).cuda().repeat(H),0).unsqueeze(0).repeat(B*W,1,1)
+class CC_module(nn.Module):
+    def __init__(self,in_dim):
+        super(CC_module, self).__init__()
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.softmax = Softmax(dim=3)
+        self.INF = INF
+        self.gamma = nn.Parameter(torch.zeros(1))
+    def forward(self, x):
+        m_batchsize, _, height, width = x.size()
+        proj_query = self.query_conv(x)
+        proj_query_H = proj_query.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height).permute(0, 2, 1)
+        proj_query_W = proj_query.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width).permute(0, 2, 1)
+        proj_key = self.key_conv(x)
+        proj_key_H = proj_key.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height)
+        proj_key_W = proj_key.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width)
+        proj_value = self.value_conv(x)
+        proj_value_H = proj_value.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height)
+        proj_value_W = proj_value.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width)
+        energy_H = (torch.bmm(proj_query_H, proj_key_H)+self.INF(m_batchsize, height, width)).view(m_batchsize,width,height,height).permute(0,2,1,3)
+        energy_W = torch.bmm(proj_query_W, proj_key_W).view(m_batchsize,height,width,width)
+        concate = self.softmax(torch.cat([energy_H, energy_W], 3))
+        #concate = concate * (concate>torch.mean(concate,dim=3,keepdim=True)).float()
+        att_H = concate[:,:,:,0:height].permute(0,2,1,3).contiguous().view(m_batchsize*width,height,height)
+        #print(concate)
+        #print(att_H) 
+        att_W = concate[:,:,:,height:height+width].contiguous().view(m_batchsize*height,width,width)
+        out_H = torch.bmm(proj_value_H, att_H.permute(0, 2, 1)).view(m_batchsize,width,-1,height).permute(0,2,3,1)
+        out_W = torch.bmm(proj_value_W, att_W.permute(0, 2, 1)).view(m_batchsize,height,-1,width).permute(0,2,1,3)
+        #print(out_H.size(),out_W.size())
+        return self.gamma*(out_H + out_W) + x
+
+class CC_decoder(nn.Module): 
+    def __init__(self,in_dim,nclass):
+        super(CC_decoder, self).__init__()
+        self.cc1=CC_module(in_dim)
+        #self.cc2=CC_module(in_dim)
+        self.head = nn.Sequential(nn.Conv2d(in_channels=in_dim*2, out_channels=32, kernel_size=3,padding=1, bias=False),nn.BatchNorm2d(32)
+                 ,nn.ReLU(inplace=True),
+                 nn.Conv2d(32, nclass, kernel_size=1, stride=1, padding=0, bias=True))
+    
+    def forward(self, x):
+         
+         a = self.cc1(x)
+         a = self.cc1(a)
+         out = torch.cat([a, x], 1)
+         out = self.head(out)
+         return out
+
+if __name__ == '__main__':
+    model = CC_module(64)
+    x = torch.randn(2, 64, 5, 6)
+    out = model(x)
+    print(out.shape)
